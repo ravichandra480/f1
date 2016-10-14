b8a155f1fc4a6cb1d6bff04b2d7adb2c239dfac1
Ravi Chandra Ravulapati <ravi.ravulapati@talktalkplc.com>
2016-10-14 12:07:25 +0100

    Distribution

--- src/components/footer/tests/footer.spec.js
+++
@@ -1,5 +0,0 @@
-describe('footer', function () {
-
-    it('should format footer', function () {
-    });
-});
--- src/tests/app.spec.js
+++
@@ -1,5 +0,0 @@
-describe('app', function () {
-
-    it('should format app', function () {
-    });
-});
